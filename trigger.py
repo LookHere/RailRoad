5ddf074d731a1c0c73b4352ed54cc84e5f301214
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-12-09 17:08:20 -0500

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -3,13 +3,7 @@ import datetime
 import RPi.GPIO as GPIO 
 import os
 
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(4, GPIO.RISING, callback=button_led, bouncetime=200)
-
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(6, GPIO.RISING, callback=button_chicken, bouncetime=200)
-
-GPIO.setup(24, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
 
 def button_led(channel):
     #
@@ -24,3 +18,14 @@ def button_chicken(channel):
     #
     
     print("Chicken button pressed!")
+
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(4, GPIO.RISING, callback=button_led, bouncetime=200)
+
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(6, GPIO.RISING, callback=button_chicken, bouncetime=200)
+
+GPIO.setup(24, GPIO.OUT)
+
+while True:
+    time.sleep(2)
