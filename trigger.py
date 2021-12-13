cad1b48021a683f331aafedad6ea967ee1a4227f
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-12-13 14:50:54 -0500

    Add servo movements

--- trigger.py
+++ trigger.py
@@ -19,22 +19,41 @@ def button_chicken(channel):
     # Detects chicken button press
     #
     print("Chicken button pressed!")
+    chicken_move()
+
+def chicken_move():
+    global p
+
+    p.start(5) # Initialization
+
+    p.ChangeDutyCycle(2.5)
+    time.sleep(1)
+    p.ChangeDutyCycle(11.5) # may need to be adjusted
+    time.sleep(1)
+    p.ChangeDutyCycle(20.5)
+    time.sleep(1)
+    p.ChangeDutyCycle(11.5) # may need to be adjusted
+    print("Chicken movement done!")
 
 # Define the GPIO pins we'll be using:
+# Inputs:
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(4, GPIO.RISING, callback=button_led, bouncetime=200)
 
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(6, GPIO.RISING, callback=button_chicken, bouncetime=200)
 
+# Outputs:
 GPIO.setup(24, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 # Set the initial output values
 GPIO.output(24, GPIO.HIGH)
+p = GPIO.PWM(18, 100)
 
 try:
     while True:
-        time.sleep(2)
+        time.sleep(5)
 except KeyboardInterrupt:
     print("Interrupted!")
     GPIO.cleanup()
